6d5e9597f71333d61cde5e2d68a93b4ed5b6a8e2
youngjun <yyj1098@gmail.com>
2025-01-15 14:01:29 +0900

    world 출력 테스트

--- src/main/java/com/myfruit/pms/controller/HelloController.java
+++ src/main/java/com/myfruit/pms/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.myfruit.pms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -10,4 +11,11 @@ public class HelloController {
     public String hello(){
         return "common/hello"; // hello.html
     }
+
+    @ResponseBody
+    @GetMapping("world")
+    public String world(){
+        return "world";
+    }
+
 }
